53bddba89c0f5c6711a551894d4e9b5e71e6ac8b
atifsatti123 <muhammadatifsatti@gmail.com>
2023-04-11 08:11:20 -0700

    first-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/hero/Hero";
+import './App.css';
+import Companies from "./components/companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/value/Value";
+import Contact from "./components/contact/Contact";
+import GetStarted from "./components/Getstarted/GetStarted";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header />
+        <Hero />
+        </div>
+        <Companies />
+        <Residencies />
+        <Value />
+        <Contact />
+        <GetStarted />
+        <Footer />
+      </div>
   );
 }
 
